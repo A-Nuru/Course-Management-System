67b669ed18c80b7315898e00b205d993a07ab39e
Nurudeen Adesina <access2abey@hotmail.com>
2022-08-01 08:31:09 +0100

    add Pages files

---
+++ Pages/CourseFormPage.cs
@@ -0,0 +1,56 @@
+﻿using CourseManagementUITestAutomation.SetUp;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementUITestAutomation.Pages
+{
+    public class CourseFormPage
+    {
+        IWebDriver _driver;
+
+        public CourseFormPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        By courseNumberField = By.Id("CourseId");
+        By courseTitleField = By.Id("CourseName");
+        By courseCreditsField = By.Id("TotalCredits");
+        By departmentIdField = By.Id("DepartmentId");
+        By createButton = By.CssSelector("input[type='submit']");
+
+        //By courseNumberErrorMsg = By.Id("input[id='CourseId']");
+        //By courseTitleErrorMsg = By.Id("input[id='CourseName']");
+        //By courseCreditsErrorMsg = By.Id("input[id='TotalCredits']");
+
+
+        public void FillInCourseNumber(string courseNumberData)
+        {
+            _driver.ClearAndSendKeys(courseNumberField, courseNumberData);
+        }
+
+        public void FillInCourseTitle(string courseTitleData)
+        {
+            _driver.ClearAndSendKeys(courseTitleField, courseTitleData);
+        }
+
+        public void FillInCourseCredits(string courseCreditData)
+        {
+            _driver.ClearAndSendKeys(courseCreditsField, courseCreditData);
+        }
+
+        public void SelectDepartmentIdDropDownMenu(string DepartmentIdData)
+        {
+            _driver.SelectOptionByText(departmentIdField, DepartmentIdData);
+        }
+        public CoursePage ClickOnCreateButton()
+        {
+            _driver.Click(createButton);
+            return new CoursePage(_driver);
+        }
+    }
+}
---
+++ Pages/CoursePage.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourseManagementUITestAutomation.SetUp;
+using OpenQA.Selenium;
+
+//using CourseManagementUITestAutomation.SetUp;
+//using OpenQA.Selenium;
+
+namespace CourseManagementUITestAutomation.Pages
+{
+    public class CoursePage
+    {
+        IWebDriver _driver;
+        public CoursePage(IWebDriver driver)
+        {
+            _driver = driver;
+
+        }
+
+        //By createNewLink = By.CssSelector("a[href='/CourseManagementSystem/Course/Create']");
+        By createNewLink = By.XPath("//a[text()='Create New']");
+        By courseTable = By.CssSelector("table[class='table']");
+        By courseTableRow = By.TagName("tr");
+
+        public CourseFormPage ClickCreateNewLink()
+        {
+            _driver.Click(createNewLink);
+            return new CourseFormPage(_driver);
+        }
+
+        public string GetCoursePageUrl()
+        {
+            return _driver.GetUrl();
+        }
+
+
+        public bool VerifyNewlyCreatedCourseRecord(string courseTiTle)
+        {
+            IList<IWebElement> courseTableDataRows = _driver.FindElement(courseTable).FindElements(courseTableRow);
+            IWebElement courseRecordExist = courseTableDataRows.FirstOrDefault(x => x.Text.Contains(courseTiTle));
+            return (courseRecordExist != null);
+
+        }
+    }
+}
