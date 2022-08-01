8c9eac19d9c93d0b7480dc4cdd45b9a01b837b4d
Nurudeen Adesina <access2abey@hotmail.com>
2022-08-01 09:27:34 +0100

    add Model files

---
+++ Model/CourseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementUITestAutomation.Model
+{
+    public class CourseModel
+    {
+        public string CourseNumber { get; set; }
+        public string Title { get; set; }
+        public string Credits { get; set; }
+        //public string DepartmentId { get; set; }
+    }
+}
